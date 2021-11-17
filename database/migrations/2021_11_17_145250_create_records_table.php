eba2cfa0019ef7d60c57f4c838b42becff48c617
Bruno Vieira Rosas <62722820+Furinha@users.noreply.github.com>
2021-11-17 16:05:13 +0100

    Création de migrations
    Création des migrations
    t_arduino
    t_room
    t_record

---
+++ database/migrations/2021_11_17_145250_create_records_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRecordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('records', function (Blueprint $table) {
+            $table->id();
+            $table->datetime('date-time-of-record');
+            $table->string('type');
+            $table->foreignId('arduino_id');
+            $table->foreignId('room_id');
+            $table->timestamps();
+
+            $table->foreign('arduino_id')->references('id')->on('arduinos')->onDelete('cascade');
+            $table->foreign('room_id')->references('id')->on('rooms')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('records');
+    }
+}
