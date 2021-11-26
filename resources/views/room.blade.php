c66a40263ccd6aa7e8e44688dc61dfa4a41b34f9
Bruno Vieira Rosas <62722820+Furinha@users.noreply.github.com>
2021-11-26 09:55:06 +0100

    Main template + chart.js

--- resources/views/room.blade.php
+++ resources/views/room.blade.php
@@ -1,3 +1,9 @@
-<h1>Room {{ $room->name}} </h1>
-<p>{{"Entrées: " . $room->records('entry')->count()}}</p>
-<p>{{"Sorties: " . $room->records('exit')->count()}}</p>
+@extends('templates/main')
+@section('script_includes')
+    
+@endsection
+@section('content')
+    <h1>Room {{ $room->name}} </h1>
+    <p>{{"Entrées: " . $room->records('entry')->count()}}</p>
+    <p>{{"Sorties: " . $room->records('exit')->count()}}</p>
+@endsection
