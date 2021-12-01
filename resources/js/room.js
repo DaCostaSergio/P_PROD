277d45a000973bb963f1b3fddd89c449d50e7ea9
Bruno Vieira Rosas <62722820+Furinha@users.noreply.github.com>
2021-12-01 14:39:23 +0100

    Implémentation de graphiques
    A faire: lier à la BD et calculer moyenne selon heure
    
    Co-Authored-By: AJAN0 <78198469+ajan0@users.noreply.github.com>

--- resources/js/room.js
+++ resources/js/room.js
@@ -1,3 +1,39 @@
-const Chart = require('chart.js')
+var Chart = require('chart.js');
 
-export default Chart;
+Chart.defaults.global.defaultFontColor = 'rgba(255, 255, 255, 1)';
+Chart.defaults.borderColor = 'rgba(255, 255, 255, 1)';
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['09h00', '09h30', '10h00', '10h30', '11h00', '11h30', '12h00', '12h30', '13h00', '13h30', '14h00', '14h30', '15h00', '15h30', '16h00', '16h30'],
+        datasets: [{
+            label: 'Nombre de visiteurs',
+            barPercentage: 1,
+            categoryPercentage: 0.5,
+            data: [11, 20, 15, 16, 25, 0, 0, 0, 0, 0, 0 ,0],
+            backgroundColor: 'rgba(255, 255, 255, 0.8)',
+            borderColor: 'rgba(255, 255, 255, 1)',
+            borderWidth: 2,
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                },
+                gridLines: {
+                    zeroLineColor: 'rgba(255, 255, 255, 1)',
+                    color: 'rgba(255, 255, 255, 0.2)'
+                }
+            }],
+            xAxes: [{
+                gridLines: {
+                    zeroLineColor: 'rgba(255, 255, 255, 1)'
+                }
+            }]
+        },
+        responsive:false
+    }
+});
